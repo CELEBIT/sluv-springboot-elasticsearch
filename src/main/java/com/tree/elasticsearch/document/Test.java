6752750c4612bda291890f0aa7bcde4901433f5d
kimkyuuuuri <rnflrnfl0633@naver.com>
2023-02-26 01:09:11 +0900

    elasticSearch 템플릿 생성 작업

--- src/main/java/com/tree/elasticsearch/document/Test.java
+++ src/main/java/com/tree/elasticsearch/document/Test.java
@@ -8,6 +8,8 @@ import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 import org.springframework.data.elasticsearch.annotations.Setting;
 
+import java.util.Date;
+
 
 @Data
 //Document에 name들도 다 들어가야함 ex. korName ..
@@ -15,6 +17,23 @@ import org.springframework.data.elasticsearch.annotations.Setting;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Setting(settingPath = "static/test_settings.json")
 public class Test {
+
+
+    @Field(type = FieldType.Text)
+    private String KORNAME_NGRAM;
+
+    @Field(type = FieldType.Text)
+    private String NAME_NGRAM;
+
+    @Field(type = FieldType.Text)
+    private String KORNAME_ENG2KOR;
+
+    @Field(type = FieldType.Text)
+    private String KORNAME_CHOSUGN;
+
+    @Field(type = FieldType.Text)
+    private String KORNAME_JAMO;
+
 
     @Field(type = FieldType.Text)
     private String name;
